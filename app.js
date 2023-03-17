67ed24953f2a16797bf3dd2b1564ca4846b0adf3
Suetin_Vladimir <Suetin-vladimir@yandex.ru>
2023-03-17 15:56:28 +0300

    создать зависимости и подключение к local serv

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.listen(PORT, () => {
+  // Если всё работает, консоль покажет, какой порт приложение слушает
+  console.log(`App listening on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
