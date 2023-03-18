d759b3d0ce42fadf0dfdf61eb09352c611f66c7c
Suetin_Vladimir <Suetin-vladimir@yandex.ru>
2023-03-18 19:04:59 +0300

    создать индекс для роутов

--- app.js
+++ app.js
@@ -2,24 +2,25 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const routes = require('./routes/index');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, BASE_PATH } = process.env;
 
 const app = express();
 
 app.use(bodyParser.json());
 
-// mongoose.connect('mongodb://localhost:27017/mestodb', {
-//   useNewUrlParser: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false,
-// });
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(routes);
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
-  console.log(`App listening on port ${PORT}`);
-});
-
-app.get('/', (req, res) => {
-  res.send('Hello');
+  console.log(`App listening on port ${BASE_PATH}`);
 });
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const userRoutes = require('./users');
+
+router.use('/users', userRoutes);
+
+router.use((req, res) => {
+  res.status(404).send({ error: 'Что то пошло не так' });
+});
