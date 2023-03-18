c0a4ef031adb999fefeb904c4878e96e7699736d
Suetin_Vladimir <Suetin-vladimir@yandex.ru>
2023-03-18 22:58:10 +0300

    создать подключение к базе данных

--- app.js
+++ app.js
@@ -2,25 +2,26 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const routes = require('./routes/index');
+const router = require('./routes/index');
 
-const { PORT = 3000, BASE_PATH } = process.env;
+const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+});
 
 const app = express();
 
 app.use(bodyParser.json());
-
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
-app.use(routes);
+app.use(router);
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
-  console.log(`App listening on port ${BASE_PATH}`);
+  console.log(`App listening on port ${PORT}`);
 });
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,5 @@ router.use('/users', userRoutes);
 router.use((req, res) => {
   res.status(404).send({ error: 'Что то пошло не так' });
 });
+
+module.exports = router;
