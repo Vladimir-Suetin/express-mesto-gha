0a3cc9019ca28edcec47e20b04c86a0a374b8f15
Suetin_Vladimir <Suetin-vladimir@yandex.ru>
2023-03-21 16:02:53 +0300

    исправить ответы на ошибки сервера

--- app.js
+++ app.js
@@ -2,24 +2,23 @@ const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const router = require('./routes/index');
+const routes = require('./routes');
 
 const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
-  // useCreateIndex: true,
-  // useFindAndModify: false,
+  //useCreateIndex: true,
+  //useFindAndModify: false,
 });
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json());
-app.use(router);
+app.use(routes);
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
