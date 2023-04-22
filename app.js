48e6844c80010477ec252c147ca38f70ecb94db4
Suetin_Vladimir <Suetin-vladimir@yandex.ru>
2023-04-22 22:32:06 +0300

    добавить логирование

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const cors = require('cors');
 const routes = require('./routes');
 const errorHandler = require('./middlewares/handleError');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT, DB_ADDRESS } = require('./config');
 
@@ -15,14 +16,17 @@ mongoose.connect(DB_ADDRESS, {
 
 const app = express();
 
-app.use(helmet());
+app.use(helmet()); // подключаем защиту заголовков
 app.use(cors({ origin: 'http://127.0.0.1:3000' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routes);
-app.use(errors());
-app.use(errorHandler);
+app.use(requestLogger); // подключаем логгер запросов
+app.use(routes); // подключаем роуты
+app.use(errorLogger); // подключаем логгер ошибок
+
+app.use(errors()); // ошибки при валидации на уровне роутов модуля celebrate
+app.use(errorHandler); // ошибка созданная при работе сервера или общая(500)
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
